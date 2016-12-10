763166b86b5ebfaf3b70b4ceec5215abd7f3d9ba
roxigo <sergey.roxigo@gmail.com>
2016-12-10 17:50:16 +0200

    ...

--- conf.py
+++ conf.py
@@ -6,7 +6,7 @@ from datetime import date
 
 source_suffix = '.rst'
 master_doc = 'index'
-project = 'Intro to BitBlox'
+project = 'Intro00 to BitBlox'
 copyright = `date.today().year` + 'BitBlox - Easiest Way To Build Landing Pages'
 #version = ''
 #release = ''
