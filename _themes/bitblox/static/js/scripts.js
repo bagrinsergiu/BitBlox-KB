6fab0545cabed9eebea4fd0a0e767d114b804b70
roxigo <sergey.roxigo@gmail.com>
2016-12-21 17:30:26 +0200

    ...

--- _themes/bitblox/static/js/scripts.js
+++ _themes/bitblox/static/js/scripts.js
@@ -1,6 +1,6 @@
 var pathname = window.location.pathname; // Returns path only
 if (pathname == '/en/latest/index.html' ||
-    pathname == '' || 
+    pathname == '/en/latest/' || 
     pathname == '/en/latest/publishing/index.html') {
 
   window.location.href = "http://help.bitblox.me/en/latest/publishing/bitblox-subdomain/index.html";
